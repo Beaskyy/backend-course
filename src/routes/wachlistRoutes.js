bcd2a3bb59429fb3c4d14e7746abc9eb341eb8f8
Beaskyy <afeez.akintobi@yahoo.com>
2026-04-25 12:17:23 +0100

    add validators

--- src/routes/wachlistRoutes.js
+++ src/routes/wachlistRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { addToWatchlist, removeFromWatchlist, updateWatchlistItem } from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authmiddleware.js";
+import { validateRequest } from "../validators/watchlistValidator.js";
+import { addToWatchlistSchema } from "../middleware/validateRequest.js";
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/", addToWatchlist);
+router.post("/", validateRequest(addToWatchlistSchema), addToWatchlist);
 
-router.put("/", updateWatchlistItem);
+router.put("/:id", updateWatchlistItem);
 
 router.delete("/:id", removeFromWatchlist)
 
